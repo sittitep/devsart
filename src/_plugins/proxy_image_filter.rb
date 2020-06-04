51ca47e9a990dfde7900b4dc1329f9d813f0e0e7
Sittitep Tosuwan <sittitep.tosuwan@gmail.com>
2020-06-04 18:15:17 +0700

    Fix nginx config

--- src/_plugins/proxy_image_filter.rb
+++ src/_plugins/proxy_image_filter.rb
@@ -1,6 +1,9 @@
+require 'uri'
+
 module Jekyll::CustomFilter
   def proxy_image_url(input, width, height, type="auto")
     if ENV["IMGPROXY"]
+      input = URI.escape(input, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
       "#{ENV['IMGPROXY_URL']}/rs:#{type}:#{width}:#{height}:0/q:100/g:sm/plain/#{absolute_url(input)}"
     else
       input
